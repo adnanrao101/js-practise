1690e0f7bc3925f9a05e31584991dbc57189ae2d
Adnan Rao <adnanejaz2874@gmail.com>
2025-05-11 17:45:25 +0500

    js done

---
+++ script.js
@@ -0,0 +1,103 @@
+// var  adnan = "900"
+// var  adnan2 = 900
+// console.log(adnan+adnan2)
+// document.write("Adnan")
+// alert("Adnan")
+
+
+
+// No 2  ILLEGAL NAMES
+
+// var 1name = "Rao SHAHBAZ"
+
+// console.log(1name);
+
+// var $name = "rao adnan"
+
+// console.log($name);
+
+// var first_name = "rao adnan"
+
+// console.log(first_name);
+
+// var firstName = "rao adnan"
+
+// console.log(firstName);
+
+
+
+// NO 3 INCREMENT AND DECREMENT
+
+// var num = 101
+// console.log(num);
+// num--
+// console.log(num);
+
+
+//No 4 Math expressions:Eliminating ambiguity
+
+// var totalCost = (1 + 3) * 4;
+
+// var resultOfComputation = (2 * 4) * 4 + 2+ 2 * 4;
+
+// console.log(resultOfComputation);
+
+
+// next 8 chapter
+ 
+// var a = 90
+// var b = 99
+// c = a+b
+// console.log(c);
+
+
+// let x = "Rao"
+// let y = 10
+
+// console.log(x+y);
+
+// This Is For INPUT USER DATA 
+// var spec = prompt("Write Your Favorite color");
+
+// document.write(spec);
+
+
+
+// var question = "What Is Your Color";
+// var defaultAnswer = "red";
+// var ans = prompt(question , defaultAnswer);
+
+// if (ans == "red"){
+//     alert("Your Favrite Color And My IS Same")
+// }
+
+// var a = +prompt("What is Your Marks in Urdu")
+
+// var b = +prompt("What is Your Marks in Math")
+
+//   var c = a + b;
+
+//    alert("The marks of two subjects is: " + c);
+
+
+var a = prompt("what is your name")
+var b = prompt("what do you do for a living")
+var c = prompt("what is your age")``
+
+var d = a + b + c;
+
+alert(d);
+
+
+// if(a > 33 ){
+//     alert("Congratiols You are Pass in Urdu Subject, And Your Number Is " + a)
+// }else{
+//     alert("You Are Fails in Urdu Subject, And Your Number Is " + a)
+// }
+// if(b > 33 ){
+//     alert("Congratiols You are Pass in Math Subject , And Your Number Is " + b)
+// }else{
+//     alert("You Are Fails in Math Subject, And Your Number Is " + b)
+// }
+
+
